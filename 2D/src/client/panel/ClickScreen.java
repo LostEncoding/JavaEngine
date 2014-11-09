6f883b3f10795e9e0483254a318f5c193f6dc688
Zach Eriksen <zmeriksen@gmail.com>
2014-11-09 13:17:38 -0600

    Adds new screens

---
+++ 2D/src/client/panel/ClickScreen.java
@@ -0,0 +1,42 @@
+package client.panel;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class ClickScreen extends Screen implements MouseListener{
+	
+	public ClickScreen(){
+		super();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ 2D/src/client/panel/KeyScreen.java
@@ -0,0 +1,27 @@
+package client.panel;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyScreen extends Screen implements KeyListener{
+	
+	public KeyScreen() {
+		super();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+	}
+
+}
